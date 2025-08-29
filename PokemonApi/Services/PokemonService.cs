a45aa802b4d9ed15be960453bc167367d513edca
MiltonChPe <miltonuzzielchableperez@gmail.com>
2025-08-28 22:26:20 -0600

    feature: AddNewEndPoints

--- PokemonApi/Services/PokemonService.cs
+++ PokemonApi/Services/PokemonService.cs
@@ -3,6 +3,8 @@ using PokemonApi.Dtos;
 using PokemonApi.Repositories;
 namespace PokemonApi.Services;
 
+using PokemonApi.Models;
+
 using PokemonApi.Mappers;
 using PokemonApi.Validators;
 
@@ -16,6 +18,19 @@ public class PokemonService : IPokemonService
         _pokemonRepository = pokemonRepository;
     }
 
+    public async Task<IList<PokemonResponseDto>> GetPokemonsByName(string name, CancellationToken cancellationToken)
+    {
+        var pokemon = await _pokemonRepository.GetPokemonsByNameAsync(name, cancellationToken);
+        return pokemon.ToResponseDto();
+     
+    }
+    public async Task<PokemonResponseDto> GetPokemonById(Guid id, CancellationToken cancellationToken)
+    {
+        var pokemon = await _pokemonRepository.GetPokemonByIdAsync(id, cancellationToken);
+        return PokemonExist(pokemon) ? pokemon.ToResponseDto() : throw new FaultException("El pokemon no existe");
+    }
+
+
     public async Task<PokemonResponseDto> CreatePokemon(CreatePokemonDto pokemonRequest, CancellationToken cancellationToken)
     {
         //fluent method
@@ -26,12 +41,17 @@ public class PokemonService : IPokemonService
             throw new FaultException("El pokemon ya existe");
         }
 
-        var pokemon = await _pokemonRepository.CreateAsync (pokemonRequest.ToModel(), cancellationToken);
+        var pokemon = await _pokemonRepository.CreateAsync(pokemonRequest.ToModel(), cancellationToken);
 
         return pokemon.ToResponseDto();
     }
+    
+    private static bool PokemonExist(Pokemon pokemon)
+    {
+        return pokemon != null;
+    }
 
-    private async Task<bool> PokemonAlreadyExist(string name,  CancellationToken cancellationToken)
+    private async Task<bool> PokemonAlreadyExist(string name, CancellationToken cancellationToken)
     {
         var pokemon = await _pokemonRepository.GetByNameAsync(name, cancellationToken);
         return pokemon != null;
