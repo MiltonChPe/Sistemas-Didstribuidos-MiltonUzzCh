c1d526772fc35b499dfd0ea8e69f4088e72e10ee
MiltonChPe <miltonuzzielchableperez@gmail.com>
2025-11-18 18:43:34 -0600

    Feature: Update grpc Endpoints

--- PlayerLockerApi/Repositories/ILockerRepository.cs
+++ PlayerLockerApi/Repositories/ILockerRepository.cs
@@ -6,4 +6,10 @@ public interface ILockerRepository
 {
     Task<Locker> CreateAsync(Locker locker, CancellationToken cancellationToken);
     Task<IEnumerable<Locker>> GetByNameAsync(string name, CancellationToken cancellationToken);
+
+    Task<Locker?> GetByIdAsync(string id, CancellationToken cancellationToken);
+
+    Task DeleteAsync(string id, CancellationToken cancellationToken);
+
+    Task UpdateAsync(Locker locker, CancellationToken cancellationToken);
 }
